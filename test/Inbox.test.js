04768d0ee28e91031bddcd65791ab6d9353321bf
Jason Pettett <jason.pettett@gmail.com>
2018-02-02 22:26:39 +1100

    Add a web3 test using promises

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,36 +5,16 @@ const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
 
-// Mocha is the test framework.
-// Uses: it (aka function)
-// describe (aka class)
-// beforeEach (aka setUp)
-
-class Car {
-  park() {
-    return 'stopped';
-  }
-
-  drive() {
-    return 'vroom';
-  }
-}
-
-
-let car;
-
 beforeEach(() => {
-  car = new Car();
+  // Get a list of all accounts
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+
+  // Use one of those accounts to deploy the contract
 });
 
-
-describe('Gokart', () => {
-  it('can park', () =>{
-    assert.equal(car.park(), 'stopped');
-  });
-
-  it('can drive', () => {
-    const car = new Car();
-    assert.equal(car.drive(), 'vroom');
-  })
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
 });
